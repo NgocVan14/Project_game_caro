62dd3c815892502b8d5b6200fc342307dce46ddd
giabao123789 <giabao2461@gmail.com>
2024-08-18 23:45:35 +0700

    the last test

--- GameCaro/Cons.cs
+++ GameCaro/Cons.cs
@@ -11,8 +11,8 @@ namespace GameCaro
         public static int CHESS_WIDTH = 30;
         public static int CHESS_HEIGHT = 30;
 
-        public static int CHESS_BOARD_WIDTH = 20;
-        public static int CHESS_BOARD_HEIGHT = 17;
+        public static int CHESS_BOARD_WIDTH = 18;
+        public static int CHESS_BOARD_HEIGHT = 19;
 
         public static int COOL_DOWN_STEP = 100;
         public static int COOL_DOWN_TIME = 1000;
